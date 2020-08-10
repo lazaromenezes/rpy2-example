4a2c52bc07689ac0b95019db541e27f70c198454
Lázaro Danillo Menezes <lazarodm@gmail.com>
2020-08-10 13:56:44 -0300

    Create gm.py

---
+++ gm.py
@@ -0,0 +1,19 @@
+# Import rpy2 packages
+from rpy2.robjects.packages import rpackages
+from rpy2.robjects.vectors import Strvector
+
+utils = rpackages.importr('utils')
+utils.chooseCRANmirror(ind=1) # Choose Mirror for installing the R packages
+
+# You can add as many packages as you want to import
+packages = ('germinationmetrics',)
+
+names_to_install = [x for x in packages if not rpackages.isinstalled(x)] # filters out the already installed ones
+
+if len(names_to_install) > 0:
+    utils.install_packages(StrVector(names_to_install))
+
+# Ymport GerminationMetrics package
+gm = rpackages.importr('germinationmetrics')
+
+help(gm)
